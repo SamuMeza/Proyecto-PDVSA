cf9b93c06398ee5669b13040e463ad1bc7a39540
Samuel <samu12meza@gmail.com>
2026-06-01 15:20:30 -0400

    FASE 0: .env creado, composer install ejecutado, composer.phar agregado

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pdvsa/sistema-mantenimiento',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e7a9e167ed1bf0c488b9355c12cc2a80ad758d8d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pdvsa/sistema-mantenimiento' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e7a9e167ed1bf0c488b9355c12cc2a80ad758d8d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
