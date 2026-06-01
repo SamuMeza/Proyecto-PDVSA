e7a9e167ed1bf0c488b9355c12cc2a80ad758d8d
Samuel <samu12meza@gmail.com>
2026-06-01 15:15:54 -0400

    fix: logo-pdvsa.jpg en lugar de .png en header

--- src/Views/partials/header.php
+++ src/Views/partials/header.php
@@ -1,7 +1,7 @@
 <header class="main-header">
     <div class="header-left">
         <button class="sidebar-toggle" id="sidebarToggle">&#9776;</button>
-        <img src="<?= \App\Core\App::BASE_PATH ?>/public/assets/images/logo-pdvsa.png" alt="PDVSA" class="header-logo" height="32">
+        <img src="<?= \App\Core\App::BASE_PATH ?>/public/assets/images/logo-pdvsa.jpg" alt="PDVSA" class="header-logo" height="32">
         <h1 class="header-title"><?= $title ?? 'Sistema PDVSA' ?></h1>
     </div>
     <div class="header-right">
